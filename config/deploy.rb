01c1e3e031591c599f036e41df7f8cbb3874d063
kiran <kiris2210@gmail.com>
2012-02-21 21:51:48 +0530

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,41 @@
-require 'capistrano'
+#require 'capistrano'
 require 'capistrano-vexxhost'
-require 'bundler/capistrano'
+ require 'bundler/capistrano'
+=begin
 
+set :application, "kirisblog"
+set :user, "kiriblog"
+set :password, "kiran@12345"
+set :deploy_to,  "/var/home/kirisblog/workspace"
+set :migrate_env, "rake db:migrate"
+set :rake, "rake"
+=end
+
+
+set :application, "kirisblog"
+set :domain, "www.kiris.in"
+set :user, "kiriblog"
+set :port, 3010
+set :repository, "/usr/home/#{user}/#{application}"
+set :scm, :none
+set :runner, user
+set :use_sudo, false
+set :deploy_via, :copy
+set :deploy_to, "/usr/local/www/#{user}/#{application}"
+set :group_writable, false
+role :app, domain
+role :web, domain
+role :db, domain, :primary => true
+
+=begin
 # Account Settings
-set :user, "kiri"
-set :password, "340t3lmoSP"
-set :domain, "kiris.in"
-set :mount_path, "/"
+set :user, "kiriblog"
+set :password, "kiran@12345"
+set :domain, "www.kiris.in"
+#set :mount_path, "/"
 set :application, "kirisblog"
 
 
-#set :repository, "/home/kiran/KIRAN/kirisblog"
-#set :local_repository, "/home/kiran/KIRAN/kirisblog"
-#set :scm, :git
-#set :deploy_via, :copy
-
-##set :application, "set your application name here"
-##set :repository,  "set your repository location here"
-
-##set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-##role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-##role :app, "your app-server here"                          # This may be the same as your `Web` server
-##role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-##role :db,  "your slave db-server here"
-
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :scm, :git
+set :repository, "git@github.com:karnati/kirisblog.git"
+=end
