f0cafa04c6b382fc5c2c0a8ee05cab11cac38ab2
kiran <kiris2210@gmail.com>
2012-02-25 12:03:48 +0530

    changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,18 @@
 require 'capistrano'
 require 'capistrano-vexxhost'
- require 'bundler/capistrano'
+
+# require 'bundler/capistrano'
+
+# Account Settings
+set :user, "kiriblog"
+set :password, "kiran@12345"
+set :domain, "www.kiris.in"
+set :mount_path, "/"
+set :application, "kirisblog"
+set :repository, "/workspace/kirisblog"
+set :local_repository, "/workspace/kirisblog"
+set :scm, :git
+set :deploy_via, :copy
 =begin
 
 set :application, "kirisblog"
@@ -12,7 +24,8 @@ set :rake, "rake"
 =end
 
 
-set :application, "kirisblog"
+=begin
+set :application, "kiris.in"
 
 set :user, "kiriblog"
 set :password, "kiran@12345"
@@ -20,9 +33,25 @@ set :scm, 'git'
 set :scm_username, 'karnati'
 set :scm_password, 'kumar123'
 set :repository, 'git@github.com:karnati/kirisblog.git'
-set :domain, "www.kiris.in"
+set :domain, "kiris.in"
 set :port, 3010
+set :deploy_to,  "/home/kiriblog/rails_apps/kirisblog"
+  set :use_sudo, false
+  set :server_hostname,  'kiris.in'
+   set :rails_env, "production"
+set(:deploy_to)         {"/home/kiriblog/rails_apps/kirisblog"}
+set(:releases_path)     { File.join(deploy_to, version_dir) }
+set(:shared_path)       { File.join(deploy_to, shared_dir) }
+set(:current_path)      { File.join(deploy_to, current_dir) }
+set(:release_path)      { File.join(releases_path, release_name) }
+  role(:web) { fetch(:server_hostname) }
+  role(:app) { fetch(:server_hostname) }
+  role(:db)  { fetch(:server_hostname) }
 
+  before 'deploy:setup', 'deploy:create_app'
+  # Mount application after setting it up
+  after 'deploy:setup', 'deploy:mount'
+=end
 
 =begin
 # Account Settings
@@ -30,7 +59,7 @@ set :user, "kiriblog"
 set :password, "kiran@12345"
 set :domain, "www.kiris.in"
 #set :mount_path, "/"
-set :application, "kirisblog"
+set :application, "www.kiris.in"
 
 
 set :scm, :git
