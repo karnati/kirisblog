9da7176d585ec0278a7280f65857e5b2d99234a6
kiran <kiris2210@gmail.com>
2012-04-14 11:57:10 +0530

    changes

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
 
-  validates :name, :email, :description, :presence => true
+  validates :name , :presence => true, :length => {:minimum => 3, :maximum => 30}
+  validates :email, :presence => true, :length => {:minimum => 5, :maximum => 50}, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+
+  validates :description, :presence => true, :length => {:minimum => 3, :maximum => 500}
 end
