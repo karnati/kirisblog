da9daeab16e9006167dbe5c4a84af7430147a486
kiran <kiris2210@gmail.com>
2012-04-17 21:26:58 +0530

    mail

--- app/mailers/enquiry_mailer.rb
+++ app/mailers/enquiry_mailer.rb
@@ -4,6 +4,6 @@ class EnquiryMailer < ActionMailer::Base
   def enquiry_email(enquiry)
     @enquiry = enquiry
      
-    mail(:to => "info@kiris.in, karnatikirankumar@gmail.com", :subject => "Enquiry Mail from Kirisin")
+    mail(:to => "info@kiris.in, karnatikirankumar@gmail.com", :subject => "#{@enquiry.subject}")
   end
 end
